9bb04b93eb035c6252b80235d41ed8bb1dd21c0b
Bishal Mishra <bishal-m001@webreinvent.com>
2024-06-04 19:54:29 +0530

    Updated: module name in index

--- src/module.ts
+++ src/module.ts
@@ -5,8 +5,8 @@ export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name: 'nuxtjs-woosmap',
+    configKey: '',
   },
   // Default configuration options of the Nuxt module
   defaults: {},
