ddd177218836bf3475fead87266c22cf867ff867
Bishal Mishra <bishal-m001@webreinvent.com>
2024-06-05 16:33:11 +0530

    Updated: base configuration and added npm link

--- src/module.ts
+++ src/module.ts
@@ -1,16 +1,17 @@
 import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import {defu} from "defu"
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export type ModuleOptions = {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
     name: 'nuxtjs-woosmap',
-    configKey: '',
+    configKey: 'nuxtjsWoosmap',
   },
   // Default configuration options of the Nuxt module
   defaults: {},
-  setup(_options, _nuxt) {
+  setup(options, nuxt) {
     const resolver = createResolver(import.meta.url)
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,5 +1,7 @@
 import { defineNuxtPlugin } from '#app'
 
-export default defineNuxtPlugin((_nuxtApp) => {
-  console.log('Plugin injected by my-module!')
+export default defineNuxtPlugin((nuxtApp) => {
+  console.log('plugin injected by woosmap-module')
+
+  
 })
